8fc0657772d18aa1557c3d91d42d4de84893962c
lansinger <66705592+lansinger@users.noreply.github.com>
2020-07-09 15:18:04 -0700

    Add files via upload

---
+++ PersonClass.java
@@ -0,0 +1,28 @@
+/*CSS 211 Summer 2020
+ * Team #2
+ * Alex Filbert
+ * Diane Lansinger (lead)
+ * Melina Perraut
+ * Zhexiu Tan
+ * Arash Yazdidoost
+ * Angie Zou
+ * 
+ * Team Project #1
+ * Building Java Programs (Reges & Step, 5th edition)
+ * Ch. 10, p. 721, Programming Exercise #3
+ * Family Database Program
+ * 
+ * Person Class represents a person and stores references
+ * to this person's mother, father and any children this
+ * person has. Person Class reads a file of names to
+ * initialize the name and parent/child relationships of 
+ * each Person object. An overall list of 'Persons' is
+ * stored as an ArrayList. Person Class is called by
+ * Main Class.
+ *
+ * Final Submission:  07-14-20
+*/
+
+public class PersonClass {
+    
+}
