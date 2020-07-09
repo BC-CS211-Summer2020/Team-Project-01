6191b6e6e2cf3b8506df2d2762bb46bc6fe352db
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-09 15:03:40 -0700

    Created Client class
    Initial version of the Client class. Represents the skeleton code for the class.

---
+++ Client.java
@@ -0,0 +1,19 @@
+/*
+* This class is used to test the Person class by instantiating a Person
+* object and returning the Person's family. User input specifies the 
+* name of the Person that will be instantiated.   
+*/
+
+public class Client{
+   public static void main(String[] args) {
+   
+      System.out.println("What's up world?");
+      
+      // Scanner object needs to be created to get user input
+      
+      // Create instance of Person object
+      
+      // Print Person object's fields in accordance to project specs
+      
+   }
+}
