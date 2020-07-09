8f574fb3e9b39b77b4c309c8fbab26a8509d69be
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-09 15:02:41 -0700

    Created Person class
    Initial version of the Person class. Represents skeleton code of the class. Compiling will result in error due to lack of return statements for now.

---
+++ Person.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Person{
+
+   /*
+   * Fields
+   */
+   
+   // Constructor
+   public Person() {
+      ;
+   }
+   
+   /*
+   * Returns an ArrayList that contains every immediate family 
+   * found in list2 
+   */
+   public ArrayList<ArrayList<String>> getListOfLists() {
+      ;
+   } 
+   
+   /*
+   * Returns the first list from the data file
+   */
+   public ArrayList<String> getFirstList() {
+      ;
+   }
+   
+   /*
+   * Returns the second list from the data file
+   */
+   public ArrayList<String> getSecondList() {
+      ;
+   }
+   
+   /*
+   * Returns the parents of the Person
+   */
+   public ArrayList<String> getParents() {
+      ;
+   }
+   
+   
+   /*
+   * Returns the Children of the Person
+   */   
+   public ArrayList<String> getChildren() {
+      ;
+   }
+}
