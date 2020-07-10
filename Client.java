d77d1d2a179f74708520c286602059b676ff56c0
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-09 17:02:25 -0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,7 +1,23 @@
-/*
-* This class is used to test the Person class by instantiating a Person
-* object and returning the Person's family. User input specifies the 
-* name of the Person that will be instantiated.   
+/*CSS 211 Summer 2020
+ * Team #2
+ * Alex Filbert
+ * Diane Lansinger (lead)
+ * Melina Perraut
+ * Zhexiu Tan
+ * Arash Yazdidoost
+ * Angie Zou
+ * 
+ * Team Project #1
+ * Building Java Programs (Reges & Step, 5th edition)
+ * Ch. 10, p. 721, Programming Exercise #3
+ * Family Database Program
+ * 
+ * Client class asks for a name and prints out the
+ * maternal, paternal, and children family line for that person
+ * according to the formatting in the textbook. Client class
+ * calls Person Class.
+ *
+ * Final Submission:  07-14-20
 */
 
 public class Client{
