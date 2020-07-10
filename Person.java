357f312adc7e20b2264882faadff26062b8ca706
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-09 17:04:53 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,28 @@
+/*CSS 211 Summer 2020
+ * Team #2
+ * Alex Filbert
+ * Diane Lansinger (lead)
+ * Melina Perraut
+ * Zhexiu Tan
+ * Arash Yazdidoost
+ * Angie Zou
+ * 
+ * Team Project #1
+ * Building Java Programs (Reges & Step, 5th edition)
+ * Ch. 10, p. 721, Programming Exercise #3
+ * Family Database Program
+ * 
+ * Person Class represents a person and stores references
+ * to this person's mother, father and any children this
+ * person has. Person Class reads a file of names to
+ * initialize the name and parent/child relationships of 
+ * each Person object. An overall list of 'Persons' is
+ * stored as an ArrayList. Person Class is called by
+ * Main class.
+ *
+ * Final Submission:  07-14-20
+*/
+
 import java.util.ArrayList;
 
 public class Person{
@@ -12,16 +37,10 @@ public class Person{
    }
    
    /*
-   * Finds the first list values and store them into an ArrayList field
+   * Finds the first and second list values and append them to their 
+   * respective ArrayList fields
    */
-   public void firstList() {
-      ;
-   }
-   
-   /*
-   * Finds the second list values and store them into an ArrayList field
-   */
-   public void secondList() {
+   public void bothLists() {
       ;
    }
    
