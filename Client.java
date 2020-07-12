775f64bce4d9f114d88e1186faeadd7404254e87
melinaperraut <67724278+melinaperraut@users.noreply.github.com>
2020-07-11 21:53:12 -0700

    Added to Client.java
    - Added way for user repeatedly input queries and quit when ready (as specified in Canvas instructions)
    - Created while loop (under if statement) to search through tudor.dat's first list (persons' names only) and check to see if user input matches any names
    - Created boolean to handle invalid user input (not found in tudor.dat)
    - Created separate method to ask whether user wants to search another name (quits if no)
    - REMAINING TO-DO: create different methods to separate the different groups of code in the main method; create instance of Person object & print Person object's fields (CANNOT DO UNTIL PERSON CLASS IS WRITTEN)

--- Client.java
+++ Client.java
@@ -23,22 +23,58 @@ import java.util.*;
 import java.io.FileNotFoundException;
 import java.io.File;
 
-public class Client{ //MP (Melina will write the Client class code)
-   
-   public static void main(String[] args) throws FileNotFoundException {
-   
-      // System.out.println("What's up world?");
-      
-      // Create scanner objects - remember to address invalid input (error message)
+// Written by Melina Perraut
+public class Client { 
+	private static boolean foundName; // (Should be inside of a method)
+	 
+	public static void main(String[] args) throws FileNotFoundException { // TO-DO: create different methods to separate different groups of code in main method
+	   // Create scanner objects 
 	   Scanner tudorFamily = new Scanner(new File("tudor.dat")); // Scanner that reads the tudor.dat file with valid names
-      Scanner console = new Scanner(System.in); // Scanner that reads user's input
+	   Scanner console = new Scanner(System.in); // Scanner that reads user's input
       
-      System.out.println("Person's name?");
-      String name = console.next();
-          
-      // Create instance of Person object
-      
-      // Print Person object's fields in accordance to project specs
-      
+	   // Ask for user input
+	   System.out.println("Enter person's name (type STOP to quit): ");
+	   String name = console.next();
+	   
+	   if (name.equals("STOP")) {
+		   System.exit(0);
+	   } else {
+		// Search through tudor.dat file for inputted name and create instance of Person object if found
+		   while (tudorFamily.hasNextLine()) { 
+			   String line = tudorFamily.nextLine();
+			   Scanner lineScan = new Scanner(line);
+			   
+			   while (lineScan.hasNext() && !(line.equals("END"))) { // For all person names (stopping at first "END" - excludes family names & duplicates)
+				   String nameInList = lineScan.next();
+				   if (nameInList.equalsIgnoreCase(name)) {
+					   foundName = true;
+					   System.out.println("Found name!"); // Test print statement
+					   // TO-DO: Create instance of Person object - CANNOT DO UNTIL PERSON CLASS IS WRITTEN
+					   // TO-DO: Print Person object's fields in accordance to project specs - CANNOT DO UNTIL PERSON CLASS IS WRITTEN
+					   // REMEMBER to call askSearchAgain 
+				   } 
+			   }			
+		   	}
+	   }
+
+		if (foundName == false) { // If user input is invalid (name entered is not in tudor.dat)
+			System.out.println("Sorry, that name was not found.");
+			askSearchAgain(console, tudorFamily);
+		}
+     
    }
+   
+	 // Checks whether the user wants to search for another name, and runs the program again if yes
+	 public static void askSearchAgain(Scanner console, Scanner tuodrFamily) throws FileNotFoundException {
+		 System.out.println();
+		 System.out.println("Would you like to search another name? (Type YES to continue and STOP to quit)");
+		 String answer = console.next();
+		 
+		 if (answer.equalsIgnoreCase("YES")) {
+			 // TO-DO: Run the program again 
+		 } else if (answer.equalsIgnoreCase("STOP")) {
+			 System.exit(0);
+		 }
+	 }
+   
 }
