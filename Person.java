a6feea307bf375d8f887017b66ef78405d3be709
Filbert-code <42334044+Filbert-code@users.noreply.github.com>
2020-07-13 15:07:49 -0700

    Update Person.java

--- Person.java
+++ Person.java
@@ -36,11 +36,6 @@ public class Person{
    private ArrayList<ArrayList<String>> familyList;
    private ArrayList<String> parents;
    private ArrayList<String> children;
-
-   // update by Zhexiu, to distingush father and mother
-   private Person father;
-   private Person mother;
-   private ArrayList<Person> childrenList;
    
    // Constructor
    public Person(String name) {
@@ -50,8 +45,6 @@ public class Person{
       familyList = new ArrayList<>();
       parents = new ArrayList<>();
       children = new ArrayList<>();
-
-      childrenList = new ArrayList<Person>();
    }
    
    /*
