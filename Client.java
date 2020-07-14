752a05d03a42db15ecf77f24723241ed395edcb6
lansinger <66705592+lansinger@users.noreply.github.com>
2020-07-14 14:30:32 -0700

    Autoformatted code indentations w/Prettier Java

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+
 /*CSS 211 Summer 2020
  * Team #2
  * Alex Filbert
@@ -24,68 +25,72 @@ import java.io.FileNotFoundException;
 import java.io.File;
 
 // Written by Melina Perraut
-public class Client { 
-   private static boolean foundName; 
-	
-   public static void main(String[] args) throws FileNotFoundException { 
+public class Client {
+   private static boolean foundName;
 
-      // Create scanner objects 
-      Scanner tudorFamily = new Scanner(new File("tudor.dat")); // Scanner that reads the tudor.dat file with valid names
+   public static void main(String[] args) throws FileNotFoundException {
+
+      // Create scanner objects
+      Scanner tudorFamily = new Scanner(new File("tudor.dat")); // Scanner that reads the tudor.dat file with valid
+                                                                // names
       Scanner console = new Scanner(System.in); // Scanner that reads user's input
       run(console, tudorFamily);
- 
+
    }
-   
+
    public static void run(Scanner console, Scanner tudorFamily) throws FileNotFoundException {
       // Written by Melina Perraut
       // Ask for user input
       System.out.print("Person's name? (Type STOP to quit): ");
       String name = console.nextLine();
-      	  
+
       if (name.equalsIgnoreCase("STOP")) {
          System.exit(0);
-      } 
-      else {
-	      tudorFamily = new Scanner(new File("tudor.dat")); // Re-initialize Scanner for tudor.dat
-	      // Search through tudor.dat file for inputted name and create instance of Person object if found
-	      while (tudorFamily.hasNextLine()) { 
-		 String line = tudorFamily.nextLine();
-		 Scanner lineScan = new Scanner(line);
+      } else {
+         tudorFamily = new Scanner(new File("tudor.dat")); // Re-initialize Scanner for tudor.dat
+         // Search through tudor.dat file for inputted name and create instance of Person
+         // object if found
+         while (tudorFamily.hasNextLine()) {
+            String line = tudorFamily.nextLine();
+            Scanner lineScan = new Scanner(line);
 
-		 while (lineScan.hasNext() && !(line.equals("END"))) { // For all person names (stopping at first "END" - excludes family names & duplicates)
-		    String nameInList = lineScan.nextLine();
-		    if (nameInList.equalsIgnoreCase(name)) {
-		       foundName = true;
-		    }           
-		 }	
-	      }  
-   	}
-      //Written by Arash Yazdidoost
+            while (lineScan.hasNext() && !(line.equals("END"))) { // For all person names (stopping at first "END" -
+                                                                  // excludes family names & duplicates)
+               String nameInList = lineScan.nextLine();
+               if (nameInList.equalsIgnoreCase(name)) {
+                  foundName = true;
+               }
+            }
+            lineScan.close();
+         }
+      }
+      // Written by Arash Yazdidoost
       if (foundName) {
-	 name = name.toLowerCase();
-    	 name = name.substring(0, 1).toUpperCase() + name.substring(1);
+         name = name.toLowerCase();
+         name = name.substring(0, 1).toUpperCase() + name.substring(1);
          Person result = new Person(name);
          result.getFamily();
-         System.out.println(result.toString());    
-	 foundName = false;
-      }
-      else { // If user input is invalid (name entered is not in tudor.dat)
-         System.out.println("Sorry, that name was not found.");	
+         System.out.println(result.toString());
+         foundName = false;
+      } else { // If user input is invalid (name entered is not in tudor.dat)
+         System.out.println("Sorry, that name was not found.");
       }
       askSearchAgain(console, tudorFamily);
    }
+
    // Written by Melina Perraut
-   // Checks whether the user wants to search for another name, and runs the program again if yes
-   public static void askSearchAgain(Scanner console, Scanner tudorFamily) throws FileNotFoundException { 
+   // Checks whether the user wants to search for another name, and runs the
+   // program again if yes
+   public static void askSearchAgain(Scanner console, Scanner tudorFamily) throws FileNotFoundException {
       System.out.println();
       System.out.println("Would you like to search another name? (Type YES to continue and STOP to quit)");
       String answer = console.nextLine();
-	System.out.println();
-		 
+      System.out.println();
+
       if (answer.equalsIgnoreCase("YES")) {
-         run(console, tudorFamily); //Run the program again 
+         run(console, tudorFamily); // Run the program again
       } else if (answer.equalsIgnoreCase("STOP")) {
-	 System.exit(0);
+         System.exit(0);
       }
    }
 }
